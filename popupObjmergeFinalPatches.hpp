ceed9e1be2d422ac8469b4383f5097f7f54008e3
Jollytrees <jollytrees@eimyous-MacBook-Pro.local>
2015-12-30 16:31:43 +0800

    merge patch part1

---
+++ popupObjmergeFinalPatches.hpp
@@ -0,0 +1,21 @@
+//
+//  popupObjmergeFinalPatches.hpp
+//  popupImage
+//
+//  Created by jollytrees on 12/28/15.
+//
+//
+
+#ifndef popupObjmergeFinalPatches_hpp
+#define popupObjmergeFinalPatches_hpp
+
+#include <stdio.h>
+#include "popupObjAlgorithm.h"
+
+class popupObjmergeFinalPatches : public popupObjAlgorithm
+{
+public:
+    virtual bool execute(popupObject *obj);
+    
+};
+#endif /* popupObjmergeFinalPatches_hpp */
