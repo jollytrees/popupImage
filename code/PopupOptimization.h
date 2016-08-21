ddd7c09a71ab7e51bded6437ae05415664a58aa8
art-programmer <liuchenwustl@gmail.com>
2016-08-21 15:16:31 -0500

    code

---
+++ code/PopupOptimization.h
@@ -0,0 +1,4 @@
+#include "PopupGraph.h"
+
+bool optimizeFoldLines(Popup::PopupGraph &popup_graph, const std::vector<std::vector<int> > &excluded_fold_line_combinations, const int num_new_fold_lines_constraint, const char optimization_type);
+//bool optimizeFoldLinePositions(Popup::PopupGraph &popup_graph);
